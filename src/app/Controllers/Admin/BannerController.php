bccf285b91a44962656fd08bdba9d05cff2208e0
Fagner Lopes <fagner.lopes@kinghost.com.br>
2022-05-12 23:42:31 -0300

    [OK] Add admin controllers

---
+++ src/app/Controllers/Admin/BannerController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\AdminBaseController;
+
+class BannerController extends AdminBaseController {
+  public function home($data) {
+    echo "<h1>Admin</h1>";
+  }
+}
